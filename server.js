64ff8cafc7991e609be4351808de533b6148671e
Amit Verma <vamit20000@gmail.com>
2025-10-22 12:14:58 +0530

    bugs fixed

--- server.js
+++ server.js
@@ -64,7 +64,15 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: "*", // Temporarily allow all origins to fix CORS issue
+  origin: function (origin, callback) {
+    // allow requests with no origin like mobile apps or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   exposedHeaders: ["Authorization"],
