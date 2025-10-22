e36d4e32a63a47cbb1ebc998b383e70ed6d5cb0a
Amit Verma <vamit20000@gmail.com>
2025-10-22 12:04:02 +0530

    cors fixed

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: "*", // Temporarily allow all origins to fix CORS issue
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   exposedHeaders: ["Authorization"],
