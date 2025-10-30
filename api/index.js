2a8f179700fa6ceb504daa423a03cd46efb2cdf9
Amit Verma <vamit20000@gmail.com>
2025-10-30 12:27:25 +0530

    api fixed

--- api/index.js
+++ api/index.js
@@ -60,6 +60,7 @@ import shopByStoreRoutes from "../routes/shopByStoreRoutes.js";
 import videoCardRoutes from "../routes/videoCardRoutes.js";
 import productSectionRoutes from "../routes/productSectionRoutes.js";
 import promoBannerRoutes from "../routes/promoBannerRoutes.js";
+import storeSectionMappingRoutes from "../routes/storeSectionMappingRoutes.js";
 
 const app = express();
 
@@ -148,6 +149,7 @@ app.use("/api/shop-by-stores", shopByStoreRoutes);
 app.use("/api/video-cards", videoCardRoutes);
 app.use("/api/product-sections", productSectionRoutes);
 app.use("/api/promo-banner", promoBannerRoutes);
+app.use("/api/store-section-mappings", storeSectionMappingRoutes);
 
 // Health check route
 app.get("/api/health", (req, res) => {
