ce39a100ffeb083843ac500412f798b4be1a0f77
Amit Verma <vamit20000@gmail.com>
2025-11-07 12:00:26 +0530

    allow all origin

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ const corsOptions = {
   credentials: true,
 })); */
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
